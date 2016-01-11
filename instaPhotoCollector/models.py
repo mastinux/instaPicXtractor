cafa1428d8f63037f6c1aa0c67b49f8457e73593
mastinux <andrea.pantaleo.93@gmail.com>
2016-01-11 01:32:58 +0100

    bootstrap integration and first approach to python-instagram

--- instaPhotoCollector/models.py
+++ instaPhotoCollector/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Media(models.Model):
+    instagram_id = models.IntegerField()
+    std_resolution_url = models.CharField(max_length=512)
+    low_resolution_url = models.CharField(max_length=512)
+    thumbnail_url = models.CharField(max_length=512)
--- instaPicXtractor/settings.py
+++ instaPicXtractor/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #mastinux
+    #my_apps
     'BITdataCollector',
     'instaPhotoCollector',
 )
@@ -103,3 +103,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(
+        BASE_DIR,
+        'static/bower_components/bootstrap/dist',
+
+    ),
+)
