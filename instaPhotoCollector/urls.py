a30c1c7ff07f247f09266d665b3aa0c6b03167df
mastinux <andrea.pantaleo.93@gmail.com>
2016-01-16 01:57:35 +0100

    added navbar with tag search and location search (by lat and lng)

--- instaPhotoCollector/urls.py
+++ instaPhotoCollector/urls.py
@@ -5,5 +5,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index_view'),
+    url(r'^search', views.search, name='search_view'),
+    url(r'^popular', views.popular, name='popular_view'),
+    url(r'^location', views.search_by_location, name='location_view'),
 ]
--- instaPhotoCollector/views.py
+++ instaPhotoCollector/views.py
@@ -7,60 +7,108 @@ import time
 
 
 def index(request):
-    # todo: understand what's first parameter for api.media_search()
+    # todo : combine BITdataCollector with instaPhotoCollector
 
+    return render(request, 'instaPhotoCollector/base.html')
+
+
+def search(request):
+    context = {}
+    if request.method == 'POST':
+        api = InstagramAPI(client_id='f72f37aa491541a79412ce319f2e061f',
+                           client_secret='7dba20c9e90b4758b558088f9422cadd')
+        image_objects = list()
+
+        searched_tag = request.POST.get('searched_tag')
+
+        print ">>> tag searched", searched_tag
+
+        response = api.tag_recent_media(40, 0, searched_tag)
+        media = response[0]
+
+        for medium in media:
+            image = {}
+            #for e in medium.__dict__:
+            #    print e
+            #for e in api.media(medium.id).__dict__:
+            #    print e
+            image['image_url'] = medium.images['standard_resolution'].url
+
+            image_objects.append(image)
+
+        context = {
+            'image_objects': image_objects,
+        }
+    return render(request, 'instaPhotoCollector/index.html', context)
+
+
+def popular(request):
     api = InstagramAPI(client_id='f72f37aa491541a79412ce319f2e061f', client_secret='7dba20c9e90b4758b558088f9422cadd')
-    images_url = list()
-    """
-    #SEARCH POPULAR
+    image_objects = list()
+
     media = api.media_popular(count=20)
     for medium in media:
-        images_url.append(medium.images['standard_resolution'].url)
-    """
-    """
-    #SEARCH BY LOCATION AND TIME
-    start = timezone.now() - timezone.timedelta(days=18)
-    end = timezone.now() - timezone.timedelta(days=16)
-
-    start_epoch = int(time.mktime(start.timetuple()))
-    end_epoch = int(time.mktime(end.timetuple()))
-
-    media = api.media_search('', 20, 40.716667, -74, start_epoch, end_epoch)
-
-    for medium in media:
-        print "\n"
+        image = {}
+        #print "\n"
         #for e in medium.__dict__:
         #    print e
-        #print medium.caption
-        print medium.location
-        print medium.created_time
-        #print medium.tags
-        #print medium.comments
-        #print medium.filter
-        #print medium.comment_count
-        #print medium.like_count
-        #print medium.link
-        #print medium.likes
-        #print medium.images
-        #print medium.users_in_photo
-        #print medium.type
-        #print medium.id
-        #print medium.user
-        images_url.append(medium.images['standard_resolution'].url)
-    """
+        image['image_url'] = medium.images['standard_resolution'].url
 
-    # SEARCH BY TAG
-    response = api.tag_recent_media(40, 0, 'roma')
-    media = response[0]
-
-    for medium in media:
-        #for e in medium.__dict__:
-        #    print e
-        #for e in api.media(medium.id).__dict__:
-        #    print e
-        images_url.append(medium.images['standard_resolution'].url)
+        image_objects.append(image)
 
     context = {
-        'images_url': images_url,
+        'image_objects': image_objects,
     }
+    return render(request, 'instaPhotoCollector/index.html', context)
+
+
+def search_by_location(request):
+    context = {}
+    if request.method == 'POST':
+        api = InstagramAPI(client_id='f72f37aa491541a79412ce319f2e061f',
+                           client_secret='7dba20c9e90b4758b558088f9422cadd')
+        image_objects = list()
+
+        lat = request.POST.get('latitude')
+        lng = request.POST.get('longitude')
+
+        start = timezone.now() - timezone.timedelta(days=14)
+        end = timezone.now() - timezone.timedelta(days=7)
+
+        start_epoch = int(time.mktime(start.timetuple()))
+        end_epoch = int(time.mktime(end.timetuple()))
+
+        # todo: understand what's first parameter for api.media_search()
+        media = api.media_search('', 20, lat, lng, start_epoch, end_epoch)
+
+        for medium in media:
+            image = {}
+            print "\n"
+            #for e in medium.__dict__:
+            #    print e
+            #print medium.caption
+            #print medium.location
+            image['location'] = medium.location
+            #print medium.created_time
+            image['created_time'] = medium.created_time
+            #print medium.tags
+            image['tags'] = medium.tags
+            #print medium.comments
+            #print medium.filter
+            #print medium.comment_count
+            #print medium.like_count
+            #print medium.link
+            #print medium.likes
+            #print medium.images
+            #print medium.users_in_photo
+            #print medium.type
+            #print medium.id
+            #print medium.user
+            image['image_url'] = medium.images['standard_resolution'].url
+
+            image_objects.append(image)
+
+        context = {
+            'image_objects': image_objects
+        }
     return render(request, 'instaPhotoCollector/index.html', context)
